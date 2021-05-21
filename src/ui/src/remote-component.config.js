61159b67bd8a39413d3b16d7b44a941290ba5b60
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-05-21 13:56:48 +0200

    Update remote packages

--- src/ui/src/remote-component.config.js
+++ src/ui/src/remote-component.config.js
@@ -4,6 +4,17 @@
 module.exports = {
   resolve: {
     react: require("react"),
+    axios: require("axios"),
+    formik: require("formik"),
+    moment: require("moment"),
+    yup: require("yup"),
+    "framer-motion": require("framer-motion"),
+    "react-datepicker": require("react-datepicker"),
+    "react-dom": require("react-dom"),
+    "react-router-dom": require("react-router-dom"),
     "@chakra-ui/react": require("@chakra-ui/react"),
+    "@chakra-ui/icons": require("@chakra-ui/icons"),
+    "@emotion/react": require("@emotion/react"),
+    "@emotion/styled": require("@emotion/styled"),
   },
 };
