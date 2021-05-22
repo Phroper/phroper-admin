174181bd429a5bd266453b3839d3bef722934dd2
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-05-22 19:05:42 +0200

    Add custom hooks to exported package

--- src/ui/src/app/plugin-system/PluginImports.js
+++ src/ui/src/app/plugin-system/PluginImports.js
@@ -1,2 +1,6 @@
+import useRequest from "./../../utils/useRequest";
+import useRequestRunner from "./../../utils/useRequestRunner";
+
 export { PluginContext } from "./PluginContext";
 export { usePluginRegister } from "./usePluginRegister";
+export { useRequest, useRequestRunner };
